fce7122ad76aaa3c3386c80f6d35b07a952263d0
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2018-11-29 16:51:10 +0800

    1. register to admin 2. edit admin display column

--- crawler_site/welcome/admin.py
+++ crawler_site/welcome/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+# from .models import Vendor, Food , VendorAdmin, FoodAdmin   #若要將admin顯示也須將其class引入
+
+# 在model採用decorator就不用每次都要引入了
+from .models import Vendor, Food  #若要將admin顯示也須將其class引入
+# # Register your models here.
+# admin.site.register(Vendor, VendorAdmin)
+# admin.site.register(Food, FoodAdmin)
+
+
+# 在model採用decorator就不用每次都要引入了
 # Register your models here.
+# admin.site.register(Vendor)
+# admin.site.register(Food)
--- crawler_site/welcome/models.py
+++ crawler_site/welcome/models.py
@@ -1,13 +1,32 @@
 from django.db import models
+from django.contrib import admin   #管理admin用
 
 # Create your models here.
 class Vendor(models.Model):
-	vendor_name = models.CharField(max_length = 20) # 攤販的名稱
-	store_name = models.CharField(max_length = 10) # 攤販店家的名稱
-	phone_number = models.CharField(max_length = 20) # 攤販的電話號碼
-	address = models.CharField(max_length = 100) # 攤販的地址
+    vendor_name = models.CharField(max_length = 20)
+    store_name = models.CharField(max_length = 10)
+    phone_number = models.CharField(max_length = 20)
+    address = models.CharField(max_length = 100)
+
+    # 覆寫 __str__ ， 它會去改變資料所要顯示的值
+    def __str__(self):
+        return self.vendor_name
 
 class Food(models.Model):
-	food_name = models.CharField(max_length = 30) # 食物名稱
-	price_name = models.DecimalField(max_digits = 3, decimal_places=0) # 食物價錢
-	food_vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE) # 代表這食物是由哪一個攤販所做的   #  當對應到model移除時一倂刪除
+    food_name = models.CharField(max_length = 30)
+    price_name = models.DecimalField(max_digits = 3, decimal_places=0)
+    food_vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)    #  當對應到model移除時一倂刪除
+
+    def __str__(self):
+        return self.food_name
+
+
+# 加此管理admin才會顯示id (key直)list_display 是固定寫法，且另外還要另外去admin import才會顯示
+# 或者使用decorator
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vendor_name')
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'food_name')
