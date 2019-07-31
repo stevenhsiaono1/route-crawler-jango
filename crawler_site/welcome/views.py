42b2ed9c7f897e5785c646f099a56a470c90f3bf
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2019-07-31 09:07:13 +0800

    test login decorator

--- crawler_site/welcome/views.py
+++ crawler_site/welcome/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Vendor
+from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+@login_required
 def showtemplate(request):
     # return HttpResponse("Hello~  for Test!")
     # 嘗試把vendor的model取出
